86a035ccea1911738f13034d3c881114e6c2c547
saman4ikdev@gmail.com <saman4ikdev@gmail.com>
2024-05-22 01:15:42 -0700

    xato  togirlandi

--- OnlineVideoCourse.Aplication/Service/EmailService.cs
+++ OnlineVideoCourse.Aplication/Service/EmailService.cs
@@ -1,15 +1,16 @@
-﻿using OnlineVideoCourse.Aplication.Interfaces;
-using MailKit.Net.Smtp;
+﻿using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
 using MimeKit.Text;
+using OnlineVideoCourse.Aplication.Interfaces;
 
-namespace OnlineVideoCourse.Aplication.Service;
+namespace OnlineVideoCourse.Application.Services;
 
 public class EmailService(IConfiguration config) : IEmailService
 {
     private readonly IConfiguration _config = config.GetSection("Email");
+
 
     public async Task SendMessageAsync(string to, string subject, string message)
     {
@@ -19,8 +20,7 @@ public class EmailService(IConfiguration config) : IEmailService
         email.Subject = subject;
         email.Body = new TextPart(TextFormat.Html) { Text = message };
 
-
-        var smtp = new SmtpClient();
+        using var smtp = new SmtpClient();
         await smtp.ConnectAsync(_config["Host"], 587, SecureSocketOptions.StartTls);
         await smtp.AuthenticateAsync(_config["EmailAddress"], _config["Password"]);
         await smtp.SendAsync(email);
