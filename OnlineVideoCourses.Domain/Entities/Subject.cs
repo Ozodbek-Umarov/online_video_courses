7557322d955ac5b87bc4c87563975b86e77f53d4
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-05-22 09:34:46 +0500

    Added Category

--- OnlineVideoCourses.Domain/Entities/Subject.cs
+++ OnlineVideoCourses.Domain/Entities/Subject.cs
@@ -5,4 +5,7 @@ public class Subject : BaseEntity
     public string Name { get; set; } = string.Empty;
     public string Description {  get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
 }
